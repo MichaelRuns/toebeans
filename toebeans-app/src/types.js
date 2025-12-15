81aaf915ed1369d113828939404d363de87542c2
Michael Vernau <102547230+MichaelRuns@users.noreply.github.com>
2025-12-14 22:38:34 -0800

    support dose/weight

--- toebeans-app/src/types.js
+++ toebeans-app/src/types.js
@@ -9,17 +9,19 @@
 /**
  * 
  * 
- * @typedef {object} Medication
+ * * @typedef {object} Medication
  * @property {string} id
  * @property {string} name
  * @property {DoseForm} doseForm
- * @property {string} doseAmount - Required dose amount (e.g., 25)
- * @property {string} doseUnit - Required dose unit (e.g., mg)
- * @property {string} frequency
- * @property {string} [concentrationValue] - Liquid: Mass value in concentration (e.g., 5)
+ * * // --- NEW WEIGHT-BASED DOSAGE FIELDS ---
+ * @property {string} dosagePerKgValue - Required dosage per weight value (e.g., 5.0)
+ * @property {string} dosageUnitPerKg - Required dosage unit per weight (e.g., mg or g)
+ * // ---------------------------------------
+ * * @property {string} frequency
+ * * @property {string} [concentrationValue] - Liquid: Mass value in concentration (e.g., 5)
  * @property {string} [concentrationMassUnit] - Liquid: Mass unit in concentration (e.g., g)
  * @property {string} [concentrationVolumeUnit] - Liquid: Volume unit in concentration (e.g., ml)
- * @property {string} [tabletSize] - Pill: Mass per unit (e.g., 100)
+ * * @property {string} [tabletSize] - Pill: Mass per unit (e.g., 100)
  */
 
 /**
